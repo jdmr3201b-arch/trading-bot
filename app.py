fd32edb7b927611627be0189f088db86d74e604a
jdmr3201b-arch <jdmr3201b@gmail.com>
2026-09-23 22:42:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if st.session_state.is_running:
     ]
 
     for selected_symbol in symbols:
-        engine.run_cycle_for_symbol(selected_symbol)
+        engine.bot.run_cycle_for_symbol(selected_symbol)
 
     time.sleep(5)
     st.rerun()
